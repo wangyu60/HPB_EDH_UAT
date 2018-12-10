b7b719431a23947d0b49cc97e1416ba524aebd39
wangyu60 <wangyu60@hotmail.com>
2018-12-10 22:30:36 +0800

    create solution

---
+++ HPB_EDH_UAT/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HPB_EDH_UAT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
